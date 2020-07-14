a004f49d34add85923e36ea07b9a06fb0f813694
hung1001 <hoanghung96cs@gmail.com>
2020-07-14 08:02:59 +0700

    Updates

---
+++ precache-manifest.52251ac0b46cad5624ec02152ea07024.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa082e613ddc064df6f02ed659f2762",
+    "url": "/meme-react/index.html"
+  },
+  {
+    "revision": "db9844edaee34ab1cae2",
+    "url": "/meme-react/static/css/2.f58e8e73.chunk.css"
+  },
+  {
+    "revision": "484261ee6cbd470109b7",
+    "url": "/meme-react/static/css/main.8aa619d3.chunk.css"
+  },
+  {
+    "revision": "db9844edaee34ab1cae2",
+    "url": "/meme-react/static/js/2.a31aeed0.chunk.js"
+  },
+  {
+    "revision": "c8182f6869249a291e1c080a6708b5e3",
+    "url": "/meme-react/static/js/2.a31aeed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484261ee6cbd470109b7",
+    "url": "/meme-react/static/js/main.2de3a0d6.chunk.js"
+  },
+  {
+    "revision": "b576c5a7b65333e22db4",
+    "url": "/meme-react/static/js/runtime-main.96b23392.js"
+  }
+]);
