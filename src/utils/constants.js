7f2006f01e13398642c1aa2da73c24f6e3d45a32
hung1001 <hoanghung96cs@gmail.com>
2020-10-06 15:23:22 +0700

    Add copy url form

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const DATA_URL = 'https://dl.dropboxusercontent.com/s/6pdwsq5ouvq0kk7/sus.json?dl=1';
+
+export {
+  DATA_URL
+};
---
+++ src/utils/helpers.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const fetchJson = async (url) => {
+  const response = await axios.get(url);
+  return response.data;
+};
+
+const getTimeUpdated = data => {
+  const { data_version } = data;
+  let year = data_version.substring(0, 4);
+  let month = data_version.substring(4, 6);
+  let day = data_version.substring(6, 8);
+  return `${day}/${month}/${year}`;
+};
+
+const setLocalStorage = (arr, name) => window.localStorage.setItem(name, JSON.stringify(arr));
+
+const getLocalStorage = (name) => JSON.parse(window.localStorage.getItem(name));
+
+export {
+  fetchJson,
+  getTimeUpdated,
+  setLocalStorage,
+  getLocalStorage
+};
