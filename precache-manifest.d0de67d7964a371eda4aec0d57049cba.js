96496971f6df9de0712fdab57b081168bf37a5c1
hung1001 <hoanghung96cs@gmail.com>
2021-12-19 17:56:40 +0700

    Updates

---
+++ precache-manifest.d0de67d7964a371eda4aec0d57049cba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2f9039ca13e081a49d513c3cfd4f9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5060d5bdeea5bc1dbc79",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ca788056c8ff3c7a1fda",
+    "url": "/static/css/main.53a6d9b1.chunk.css"
+  },
+  {
+    "revision": "5060d5bdeea5bc1dbc79",
+    "url": "/static/js/2.fa41aab6.chunk.js"
+  },
+  {
+    "revision": "7b3ab6fd10ab6543e34068996d1ff58d",
+    "url": "/static/js/2.fa41aab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca788056c8ff3c7a1fda",
+    "url": "/static/js/main.74b261ec.chunk.js"
+  },
+  {
+    "revision": "621fd10895ae405bcbaa",
+    "url": "/static/js/runtime-main.fc7b2bb8.js"
+  }
+]);
